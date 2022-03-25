843c4a4eedcdc67c709d8832e9a55ac31b3ae61d
Nils Putnins <nils@influent.solutions>
2022-03-25 17:30:58 +0200

    Outputting vehicles

--- main.py
+++ main.py
@@ -6,13 +6,28 @@ headers = {
     'User-Agent': constant.USERAGENT
 }
 
+postData = {
+    "topt[8][min]": "",
+    "topt[8][max]": "",
+    "topt[18][min]": "2016",
+    "topt[18][max]": "",
+    "topt[15][min]": "2.5",
+    "topt[15][max]": "",
+    "opt[34]": "",
+    "opt[35]": "497",
+    "opt[32]": "",
+    "opt[17]": "",
+    "sid": "/en/transport/cars/today/filter/"
+}
+
 def getClassifieds(url = constant.BASE, filters = []):
-    page = requests.get(url, headers=headers, timeout=constant.TIMEOUT)
+    page = requests.post(url, headers = headers, data = postData, timeout = constant.TIMEOUT)
     if page.status_code == 200:
         soup = BeautifulSoup(page.content, 'html.parser')
 
-        vehicles = soup.find_all('table')
-        print(vehicles)
+        vehicles = soup.findAll("tr", {"id" : lambda L: L and L.startswith('tr_')})
+        for vehicle in vehicles:
+            print(vehicle.text)
     else:
         print("Error fetching classifieds.")
 
